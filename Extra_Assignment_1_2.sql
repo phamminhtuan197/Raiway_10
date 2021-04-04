93a0b231362b65131851de0595abe1beaea201a0
phamminhtuan197 <phamminhtuan197@gmail.com>
2021-04-04 19:25:31 +0700

    Update Extra_Assignment_1_2.sql
    sua bai 3

--- Extra_Assignment_1_2.sql
+++ Extra_Assignment_1_2.sql
@@ -14,5 +14,18 @@ CREATE TABLE IF NOT EXISTS Trainee
     ET_English		INT UNSIGNED CHECK(ET_English <=50) NOT NULL,
     Training_Class	VARCHAR(20) NOT NULL,
     Evaluation_Note	TEXT,
-    VTI_Account		VARCHAR(50) NOT NULL UNIQUE KEY
+    VTI_Account		VARCHAR(50) NOT NULL UNIQUE
 );
+INSERT INTO TraineeID (Full_Name, Birth_Date, Gender, ET_IQ, ET_Gmath, ET_English, Training_Class, Evaluation_Note, VTI_Account) VALUES
+('Chu Ngọc Minh Châu','30/08/2008', 'female', 10, 15, 20, '7B', 'Đang học', 'chungocminhchau'),
+('Đặng Ngọc Minh Châu','17/08/2008', 'female', 15, 20, 25, '7A', 'Đang học', 'dangngocminhchau'),
+('Đỗ Minh Châu','06/06/2009', 'female', 11, 12, 25, '6B', 'Đang học', 'dominhchau'),
+('Lê Thị Hoàng Châu','10/11/2007', 'female', 10, 15, 20, '8B', 'Đang học', 'lethihoangchau'),
+('Chu Ngọc Minh Châu','30/08/2008', 'female', 10, 15, 20, '6B', 'Đang học', 'chungocminhchau'),
+('Chu Ngọc Minh Châu','30/08/2008', 'female', 10, 15, 20, '6B', 'Đang học', 'chungocminhchau'),
+('Chu Ngọc Minh Châu','30/08/2008', 'female', 10, 15, 20, '6B', 'Đang học', 'chungocminhchau'),
+('Chu Ngọc Minh Châu','30/08/2008', 'female', 10, 15, 20, '6B', 'Đang học', 'chungocminhchau'),
+('Chu Ngọc Minh Châu','30/08/2008', 'female', 10, 15, 20, '6B', 'Đang học', 'chungocminhchau'),
+('Chu Ngọc Minh Châu','30/08/2008', 'female', 10, 15, 20, '6B', 'Đang học', 'chungocminhchau'),
+
+
