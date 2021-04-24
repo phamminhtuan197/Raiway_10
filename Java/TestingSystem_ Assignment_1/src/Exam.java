77b567a935e921e541a1e548ead4adc61d34c206
phamminhtuan197 <81756348+phamminhtuan197@users.noreply.github.com>
2021-04-24 22:24:45 +0700

    Create Exam.java

---
+++ Java/TestingSystem_ Assignment_1/src/Exam.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Exam {
+	int					id;
+	int					code;
+	String				title;
+	CategoryQuestion	category;
+	int					duration;
+	Account				creator;
+	Date				createDate;
+}
