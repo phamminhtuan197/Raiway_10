40fb4b100565d06e9898dbef4130db65c5b2e716
phamminhtuan197 <81756348+phamminhtuan197@users.noreply.github.com>
2021-04-27 17:06:35 +0700

    Update TypeQuestion.java

--- Java/TestingSystem_ Assignment_1/src/TypeQuestion.java
+++ Java/TestingSystem_ Assignment_1/src/TypeQuestion.java
@@ -2,4 +2,13 @@
 public class TypeQuestion {
 	int			id;
 	TypeName	name;
+	
+	TypeQuestion(int id, TypeName name) {
+		this.id=id;
+		this.name=name;
+	}
+	
+	public String toString() {
+		return "TypeQuestion [id=" + id + ", name=" + name + "]";
+	}
 }
