f14aacf045fd84aa886dc4b1aa233f14a6a14d3a
phamminhtuan197 <81756348+phamminhtuan197@users.noreply.github.com>
2021-04-27 17:06:27 +0700

    Update Position.java

--- Java/TestingSystem_ Assignment_1/src/Position.java
+++ Java/TestingSystem_ Assignment_1/src/Position.java
@@ -2,4 +2,13 @@
 public class Position {
 	int				id;
 	PositionName	name;
+	
+	Position(int id, PositionName name) {
+		this.id=id;
+		this.name=name;
+	}
+	
+	public String toString() {
+		return "Position [id=" + id + ", name=" + name + "]";
+	}
 }
