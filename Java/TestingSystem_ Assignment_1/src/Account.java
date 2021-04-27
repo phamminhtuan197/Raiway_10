001a93a6deea00cbad9bbcb28400dc2ebd1f1812
phamminhtuan197 <81756348+phamminhtuan197@users.noreply.github.com>
2021-04-27 17:06:13 +0700

    Update Account.java

--- Java/TestingSystem_ Assignment_1/src/Account.java
+++ Java/TestingSystem_ Assignment_1/src/Account.java
@@ -1,5 +1,4 @@
 import java.util.Date;
-
 public class Account {
 	int			id;
 	String		email;
@@ -8,4 +7,19 @@ public class Account {
 	Department	department;
 	Position	position;
 	Date		createDate;
+	Group[]		groups;
+	
+	Account(int id,String email, String userName, String fullName, Department department, Position position){
+		this.id=id;
+		this.email=email;
+		this.userName=userName;
+		this.fullName=fullName;
+		this.department=department;
+		this.position=position;
+	}
+	
+	public String toString() {
+		return "Account [id=" + id + ", email=" + email + ", userName=" + userName + ", fullName=" + fullName
+				+ ", department=" + department + ", position=" + position + ", createDate=" + createDate + ", groups=" + groups + "]";
+	}
 }
