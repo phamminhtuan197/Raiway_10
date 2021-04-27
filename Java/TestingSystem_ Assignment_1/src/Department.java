8039ea81d9c848022fba4d5117f33b19c8e202d4
phamminhtuan197 <81756348+phamminhtuan197@users.noreply.github.com>
2021-04-27 17:06:20 +0700

    Update Department.java

--- Java/TestingSystem_ Assignment_1/src/Department.java
+++ Java/TestingSystem_ Assignment_1/src/Department.java
@@ -2,4 +2,14 @@
 public class Department {
 	int 			id;
 	DepartmentName	name;
+	
+	Department(int id, DepartmentName name) {
+		this.id=id;
+		this.name=name;
+	}
+	
+	public String toString() {
+		return "[id=" + id + ", name=" + name + "]";
+	}
 }
+
