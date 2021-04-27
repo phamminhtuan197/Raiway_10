7da92a1994b335d16352b3e7ab524fca8f1ada04
phamminhtuan197 <81756348+phamminhtuan197@users.noreply.github.com>
2021-04-27 17:06:22 +0700

    Update Exam.java

--- Java/TestingSystem_ Assignment_1/src/Exam.java
+++ Java/TestingSystem_ Assignment_1/src/Exam.java
@@ -8,4 +8,19 @@ public class Exam {
 	int					duration;
 	Account				creator;
 	Date				createDate;
+	Question[]			questions;
+
+	Exam(int id, int code, String title, CategoryQuestion category, int duration, Account creator){
+		this.id=id;
+		this.code=code;
+		this.title=title;
+		this.category=category;
+		this.duration=duration;
+		this.creator=creator;
+	}
+	
+	public String toString() {
+		return "Exam [id=" + id + ", code=" + code + ", title=" + title + ", category="
+				+ category + ", duration=" + duration + ", creator=" + creator + ", createDate=" + createDate + ", questions=" + questions + "]";
+	}
 }
